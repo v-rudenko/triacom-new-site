a9b4cdaf37a3e289981a2338dc089adf248dbc5e
Viktor Rudenko <v-rudenko@triacom.ua>
2023-04-28 00:14:31 +0300

    created empty contactForm component

--- src/components/UI/contactForm/ContactForm.tsx
+++ src/components/UI/contactForm/ContactForm.tsx
@@ -1,11 +1,17 @@
-import { Box } from '@mui/material'
-import Banner from "./banner/banner.jpg"
+import { Box } from "@mui/material";
+import Banner from "./banner/banner.jpg";
+import classes from "./ContactForm.module.scss";
 // type Props = {}
 
 const ContactForm = () => {
   return (
-    <Box><img src={Banner} height="500px" alt="" /></Box>
-  )
-}
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      {/* <img className={classes.banner} src={Banner} height="500px" alt="" /> */}
+      <div className={classes.banner_div}>
+        <div>Baboon</div>
+      </div>
+    </Box>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
