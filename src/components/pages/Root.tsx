a9351864844e89867a4eb8c889b0a832a1d8426c
Viktor Rudenko <v.rudenko@triacom.ua>
2023-05-13 21:15:21 +0300

    chat removed

--- src/components/pages/Root.tsx
+++ src/components/pages/Root.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 import BackgroundImage from "../UI/background/BackgroundImage"
 import Navbar from "../UI/navigation/Navbar"
 
+
 const RootLayout = () => {
   return (
     <>
