4342faa0ca40af3d7d30275960f1a346c3942792
Viktor Rudenko <v.rudenko@triacom.ua>
2023-05-13 19:03:49 +0300

    later

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import AboutCompany from "./components/pages/about_company/AboutCompany";
 import Contacts from "./components/pages/contacts/Contacts";
 import MainPage from "./components/pages/main_page/MainPage";
 import Questions from "./components/pages/questions/Questions";
-import Admin from "./components/admin/Admin";
 import Navbar from "./components/UI/navigation/Navbar";
+import BackgroundImage from "./components/UI/background/BackgroundImage";
 
 function App() {
   return (
     <>  
       <Navbar />
+      <BackgroundImage />
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/about-company" element={<AboutCompany />}></Route>
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Admin from "./components/admin/Admin.tsx";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
