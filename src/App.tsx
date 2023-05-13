fae5f945cc6504f2cff9456dd006f19a902ebe36
Viktor Rudenko <v.rudenko@triacom.ua>
2023-05-13 16:21:52 +0300

    fix for router fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import "./App.scss";
 import Navbar from "./components/UI/navigation/Navbar";
 
 import { Routes, Route } from "react-router-dom";
-
-import BackgroundImage from "./components/UI/background/BackgroundImage";
+import AboutCompany from "./components/pages/about_company/AboutCompany";
+import Contacts from "./components/pages/contacts/Contacts";
+import MainPage from "./components/pages/main_page/MainPage";
+import Questions from "./components/pages/questions/Questions";
 
 function App() {
   return (
     <>
       <Navbar />
-      <BackgroundImage />
+      
       <Routes>
+        <Route
+          path="/"
+          element={<MainPage />}
+        ></Route>
+        <Route
+          path="/about-company"
+          element={<AboutCompany />}
+        ></Route>
+        <Route
+          path="/questions"
+          element={<Questions />}
+        ></Route>
+        <Route path="/contacts" element={<Contacts />}></Route>
       </Routes>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import AboutCompany from "./components/pages/about_company/AboutCompany";
-import Contacts from "./components/pages/contacts/Contacts";
-import MainPage from "./components/pages/main_page/MainPage";
-import Questions from "./components/pages/questions/Questions";
-
 import "./index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Admin from "./components/admin/Admin.tsx";
@@ -16,15 +11,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <BrowserRouter>
       <Routes>
         {/* <App /> */}
-        <Route path="/triacom-new-site" element={<App />}>
-          {" "}
-          <Route path="/triacom-new-site/" element={<MainPage />}></Route>
-          <Route path="/triacom-new-site/about-company" element={<AboutCompany />}></Route>
-          <Route path="/triacom-new-site/questions" element={<Questions />}></Route>
-          <Route path="/triacom-new-site/contacts" element={<Contacts />}></Route>
-        </Route>
-        <Route path="/triacom-new-site/admin" element={<Admin />}></Route>
-
+        <Route path="/triacom-new-site/*" element={<App />}></Route>
+        <Route path="/triacom-new-site/admin/*" element={<Admin />}></Route>
         {/* <Route path="/triacom-new-site/about-company" element={<Admin />}></Route>
         <Route path="/triacom-new-site/contacts" element={<Admin />}></Route> */}
       </Routes>
