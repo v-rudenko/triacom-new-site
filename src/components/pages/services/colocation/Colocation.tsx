1acc94629affccda6696920f9b81b0c4acdefc3b
Viktor Rudenko <darthvitek@gmail.com>
2023-05-23 16:18:46 +0300

    added colocation page

--- src/components/pages/services/colocation/Colocation.tsx
+++ src/components/pages/services/colocation/Colocation.tsx
@@ -1,21 +1,39 @@
-import { Box, styled } from "@mui/material";
-import colocationBanner from "./images/banner.avif";
+import { Box, Typography, styled } from "@mui/material";
+import colocationBanner from "./images/servers.png";
 import classes from "./Colocation.module.scss";
+import BackgroundImage from "../../../UI/background/BackgroundImage";
+import bg from "./images/bg.jpg";
+
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ColocationTable from "./ColocationTable";
 
 const CentringBox = styled(Box)({
   display: "flex",
-  justifyContent: " space-around",
-  margin: "70px 150px 70px",
+  flexDirection: "column",
+  alignItems: "center",
+  // justifyContent: " space-around",
+  margin: "30px 150px",
+  gap: 25,
+  // background: "#D6E6F2"
 });
 
-const TextBox = styled(Box)({
-  width: "560px",
-  // height: "420.45px",
-  // border: "1px solid red",
+const BenefitsBox = styled(Box)({
+  display: "inline-flex",
+  flexDirection: "column",
+  alignSelf: "flex-start",
+  // width: "100%",
+  marginLeft: "100px"
+});
+
+const CheckboxTextBox = styled(Box)({
+  display: "flex",
+  marginTop: "20px",
+  alignItems: "flex-start",
+  gap: 8
 });
 
 const ImageBox = styled(Box)({
-  width: "600px",
+  // width: "600px",
   // borderRadius: "22px"
   // width: "558px",
   // height: "386px",
@@ -24,13 +42,63 @@ const ImageBox = styled(Box)({
 
 const Colocation = () => {
   return (
-    <CentringBox>
-      <TextBox></TextBox>
+    <>
+      {/* <BackgroundImage /> */}
+      <div>
+        <img className={classes.bg} src={bg} alt="" />
+      </div>
+      <CentringBox>
+        <Box>
+          <Typography sx={{ fontWeight: 700 }} variant="h3" component={"h1"}>
+            Послуга колокейшн - розміщення серверів
+          </Typography>
+        </Box>
+
+        <ImageBox>
+          <img
+            className={classes.banner}
+            src={colocationBanner}
+            width={"1200px"}
+            height={"400px"}
+          />
+        </ImageBox>
+        <Box>
+          <Typography sx={{ fontSize: 18 }}>
+            Triangulum має власний дата-центр, для розміщення серверного та
+            телекомунікаційного обладнання наших клієнтів
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: 10}}>
+          <BenefitsBox>
+            <Typography variant="h5">Переваги нашого сервісу</Typography>
+            <CheckboxTextBox>
+              <CheckCircleIcon className={classes.checkbox}/>
+              <Typography>Безперебійне електропостачання</Typography>
+            </CheckboxTextBox>
+            <CheckboxTextBox>
+              <CheckCircleIcon className={classes.checkbox}/>
+              <Typography>Можливість доступу до обладнання 24/7</Typography>
+            </CheckboxTextBox>
+            <CheckboxTextBox>
+              <CheckCircleIcon className={classes.checkbox}/>
+              <Typography>Стабільний температурний режим</Typography>
+            </CheckboxTextBox>
+            <CheckboxTextBox>
+              <CheckCircleIcon className={classes.checkbox}/>
+              <Typography>Можливість виділення додаткової мережі ip адрес</Typography>
+            </CheckboxTextBox>
+            <CheckboxTextBox>
+              <CheckCircleIcon className={classes.checkbox}/>
+              <Typography>Цілодобова технічна підтримка</Typography>
+            </CheckboxTextBox>
+            
+          </BenefitsBox>
+          <Box sx={{width: "700px", float: "right"}}><ColocationTable /></Box>
+        </Box>
+        
+      </CentringBox>
       
-      <ImageBox>
-        <img className={classes.banner} src={colocationBanner} height="500px" />
-      </ImageBox>
-    </CentringBox>
+    </>
   );
 };
 
---
+++ src/components/pages/services/colocation/ColocationTable.tsx
@@ -0,0 +1,57 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(
+  name: number | string,
+  power: number | string,
+  capacity: number | string,
+  size: number | string
+) {
+  return { name, power, capacity, size };
+}
+
+const rows = [
+  createData("Потужність", "000", "000", "000"),
+  createData("Місткість", "000", "000", "000"),
+  createData("Розмір", "000", "000", "000"),
+  createData("Абонентська плата", "Індивідуально", "Індивідуально", "Індивідуально"),
+];
+
+function ColocationTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650, maxWidth: "100%" }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="right">Розміщення сервера</TableCell>
+            <TableCell align="right">Оренда стійки</TableCell>
+            <TableCell align="right">Оренда шафи</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.power}</TableCell>
+              <TableCell align="right">{row.capacity}</TableCell>
+              <TableCell align="right">{row.size}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default ColocationTable;
